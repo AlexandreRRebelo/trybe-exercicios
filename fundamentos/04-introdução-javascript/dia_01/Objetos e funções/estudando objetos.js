214bad89adaed19a5b8dd34bc5d473dde1256ff1
AlexandreRR <alex.r.rebelo@gmail.com>
2022-09-28 15:35:14 -0300

    okay

---
+++ fundamentos/04-introdução-javascript/dia_01/Objetos e funções/estudando objetos.js
@@ -0,0 +1,28 @@
+let pizzas = {
+    sabor: "palmito",
+    preco: 39.90,
+    bordaCatupiry: true
+
+};
+ for (let key in pizzas) {
+    console.log(pizzas[key]);
+ }
+
+ let pizzasDoces = ["chocolate", "banana", "morango"];
+ for (let key in pizzasDoces) {
+    console.log(key,pizzasDoces[key]);
+ }
+
+ let food = ['hamburguer', 'bife', 'acarajé'];
+for (let value of food) {
+  console.log(value);
+};
+
+let names = {
+  person1: 'João',
+  person2: 'Maria',
+  person3: 'Jorge',
+}
+for (let Ola of names) {
+    console.log(value);
+  };
